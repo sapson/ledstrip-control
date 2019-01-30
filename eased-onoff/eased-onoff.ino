4022b5d32e3b049d3dca3f3f28b6f51a2277046a
Sapson <sapson@telenet.be>
2019-01-30 08:06:08 +0100

    working with PIR and POT

--- eased-onoff/eased-onoff.ino
+++ eased-onoff/eased-onoff.ino
@@ -16,7 +16,8 @@ QuarticEase ease;
 const byte ledStrip1Pin = 1; // Connection for the ledstrip (via ULN2803) PB1
 const byte ledStrip2Pin = 0; // Connection for the ledstrip (via ULN2803) PB0
 const byte switchPin = 4;    // pin number to connect the switch(es) to PB4
-const byte pirPin = 3;       // Optional PIR PB3
+const byte pirPin = 2;       // Optional PIR PB2
+const byte potPin = 3;       // Optional Potentiometer to PB3
 
 double easedPosition, t = 0;
 
@@ -27,9 +28,11 @@ int lastSwitchState = LOW; // the previous reading from the input pin
 unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 
+int maxValue = 255;       // the max value for the led brighthness
+
 void setup()
 {
-  ease.setDuration(5); //has to be made dynamic later
+  ease.setDuration(2); //has to be made dynamic later
   ease.setTotalChangeInPosition(255);
 
   /*
@@ -40,7 +43,7 @@ void setup()
   */
 
   // set input and output mode via DDRB register (instead of pinMode)
-  DDRB |= (0 << DDB4) | (0 << DDB3) | (1 << DDB1) | (1 << DDB0);
+  DDRB |= (0 << DDB4) | (0 << DDB3) | (0 << DDB2) | (1 << DDB1) | (1 << DDB0);
 
   // Run once after reboot of the arduino
   smoothOn();
@@ -53,14 +56,29 @@ void loop()
   t = 0;
   // read the state of the door switches
   int reading = LOW;
-  
+
+  // read the state of the Potentiometer
+  int potReading = (int) analogRead(potPin) / 4;
+  // if brighthness has changed, and led is already on at maxvalue, adapt the brighthness
+  if (potReading != maxValue) {
+    maxValue = potReading;
+    if (easedPosition > 0) {
+       analogWrite(ledStrip1Pin, (unsigned char)maxValue);
+       analogWrite(ledStrip2Pin, (unsigned char)maxValue);
+    }
+  }
+  // reset reading to LOW
+  reading = LOW;
+
+  // if SWITCH is OPEN then set HIGH
   if (bitGet(PINB,BIT(4)))
   {
     reading = HIGH;
   } 
-  else
+  // if PIR detected movement then set HIGH
+  if (bitGet(PINB,BIT(2)))
   {
-    reading = LOW;
+    reading = HIGH;
   }
 
   if (reading != lastSwitchState)
@@ -78,6 +96,7 @@ void loop()
     if (reading != switchState)
     {
       switchState = reading;
+      maxValue = potReading;
 
       // check if the pushbutton is pressed. If it is, the switchState is HIGH:
       if ((switchState == HIGH) and (easedPosition == 0))
@@ -97,19 +116,27 @@ void loop()
   lastSwitchState = reading;
 }
 
+void rapidOn() {
+    // turn LED strip on without any smoothing
+    analogWrite(ledStrip1Pin, (unsigned char)maxValue);
+    analogWrite(ledStrip2Pin, (unsigned char)maxValue);
+    delay(5);
+    easedPosition = maxValue;
+}
+
 void smoothOn()
 {
   // turn LED strip on:
 
-  while (easedPosition < 255)
+  while (easedPosition < maxValue)
   {
     easedPosition = ease.easeInOut(t);
     analogWrite(ledStrip1Pin, (unsigned char)easedPosition);
     analogWrite(ledStrip2Pin, (unsigned char)easedPosition);
     t += 0.005;
     delay(5);
-  }
-  easedPosition = 255;
+  } //
+  easedPosition = maxValue;
 
   // PORTB |= (1<<ledStrip2Pin) | (1<<ledStrip1Pin);
 }
